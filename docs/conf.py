20268cd9edeca59cdb6e1dc80a02b7cb23f4692b
Karol Warda <karx132@gmail.com>
2024-01-22 18:05:44 +0100

    Update docs
    From now on docs only contain html documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(".."))
 
-project = 'InfiniteRunner (chrome dinosaur game replica)'
+project = 'InfiniteRunner game (chrome dinosaur replica)'
 copyright = '2024, Karol Warda'
 author = 'Karol Warda'
 release = '1.0'
